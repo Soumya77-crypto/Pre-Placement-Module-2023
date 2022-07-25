3b8d7e1ebdb9d90a1f8c5f15fcc80513bceb2cfb
Soumya77-crypto <65383867+Soumya77-crypto@users.noreply.github.com>
2022-07-25 16:46:14 +0530

    commit

---
+++ ALGO I/Day 13/NumberOf1Bits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+#         y=str(n)
+#         x=list(map(int,y.strip()))
+#         return (x.count(1))
+        
+          return bin(n).count('1')
+        
